de1f01829560ca21545ac17cafe36167c8c3d2b6
jeroendm <jeroendemaeyer@live.be>
2022-04-03 16:26:29 +0200

    move global gui state into struct

--- main/gui.c
+++ main/gui.c
@@ -14,14 +14,22 @@
 const int WIDTH = 400;
 const int HEIGHT = 200;
 
-Display *dpy;
-Window root, win;
-XEvent evt;
-GC gc;
+struct GlobalGUIState {
+	Display* display;
+	Window root;
+	Window win1;
+	GC gc;
+} GLOBAL_STATE;
+
 
 void x11_draw_line(int x1, int y1, int x2, int y2)
 {
-	XDrawLine(dpy, win, gc, x1, y1, x2, y2);
+	XDrawLine(
+		GLOBAL_STATE.display,
+		GLOBAL_STATE.win1,
+		GLOBAL_STATE.gc,
+		x1, y1, x2, y2
+	);
 }
 
 int main(int argc, char *argv[])
@@ -49,69 +57,71 @@ int main(int argc, char *argv[])
 	XWMHints wmhints;
 
 	setlocale(LC_ALL, "");
-	dpy = XOpenDisplay(NULL);
-	WM_DELETE = XInternAtom(dpy, "WM_DELETE_WINDOW", 0);
-	_NET_WM_NAME = XInternAtom(dpy, "_NET_WM_NAME", 0);
-	_NET_WM_ICON_NAME = XInternAtom(dpy, "_NET_WM_ICON_NAME", 0);
-	UTF8_STRING = XInternAtom(dpy, "UTF8_STRING", 0);
-	black = BlackPixel(dpy, 0);
-	white = WhitePixel(dpy, 0);
+	// dpy = XOpenDisplay(NULL);
+	GLOBAL_STATE.display = XOpenDisplay(NULL);
+	WM_DELETE = XInternAtom(GLOBAL_STATE.display, "WM_DELETE_WINDOW", 0);
+	_NET_WM_NAME = XInternAtom(GLOBAL_STATE.display, "_NET_WM_NAME", 0);
+	_NET_WM_ICON_NAME = XInternAtom(GLOBAL_STATE.display, "_NET_WM_ICON_NAME", 0);
+	UTF8_STRING = XInternAtom(GLOBAL_STATE.display, "UTF8_STRING", 0);
+	black = BlackPixel(GLOBAL_STATE.display, 0);
+	white = WhitePixel(GLOBAL_STATE.display, 0);
 	x = 500;
 	y = 500;
-	root = RootWindow(dpy, 0);
-	win = XCreateSimpleWindow(dpy, root, 100, 50, WIDTH, HEIGHT, 0, 0, white);
-	XChangeProperty(dpy, win, _NET_WM_NAME, UTF8_STRING, 8, 0, str, strlen(str));
-	XChangeProperty(dpy, win, XA_WM_NAME, XA_STRING, 8, 0, str, strlen(str));
-	XChangeProperty(dpy, win, _NET_WM_ICON_NAME, UTF8_STRING, 8, 0, str, strlen(str));
-	XChangeProperty(dpy, win, XA_WM_ICON_NAME, XA_STRING, 8, 0, str, strlen(str));
-	XSelectInput(dpy, win,
+	GLOBAL_STATE.root = RootWindow(GLOBAL_STATE.display, 0);
+	GLOBAL_STATE.win1 = XCreateSimpleWindow(GLOBAL_STATE.display, GLOBAL_STATE.root, 100, 50, WIDTH, HEIGHT, 0, 0, white);
+	XChangeProperty(GLOBAL_STATE.display, GLOBAL_STATE.win1, _NET_WM_NAME, UTF8_STRING, 8, 0, str, strlen(str));
+	XChangeProperty(GLOBAL_STATE.display, GLOBAL_STATE.win1, XA_WM_NAME, XA_STRING, 8, 0, str, strlen(str));
+	XChangeProperty(GLOBAL_STATE.display, GLOBAL_STATE.win1, _NET_WM_ICON_NAME, UTF8_STRING, 8, 0, str, strlen(str));
+	XChangeProperty(GLOBAL_STATE.display, GLOBAL_STATE.win1, XA_WM_ICON_NAME, XA_STRING, 8, 0, str, strlen(str));
+	XSelectInput(GLOBAL_STATE.display, GLOBAL_STATE.win1,
 			ButtonPressMask | ButtonReleaseMask | ExposureMask | StructureNotifyMask);
 	font =
-		XCreateFontSet(dpy, "-misc-*-medium-r-normal--14-*-*-*-*-*-*",
+		XCreateFontSet(GLOBAL_STATE.display, "-misc-*-medium-r-normal--14-*-*-*-*-*-*",
 				&missing_charset_list_return, &missing_charset_count_return,
 				&def_string_return);
-	gc = XCreateGC(dpy, win, 0, 0);
-	XSetBackground(dpy, gc, white);
-	XSetForeground(dpy, gc, black);
+	GLOBAL_STATE.gc = XCreateGC(GLOBAL_STATE.display, GLOBAL_STATE.win1, 0, 0);
+	XSetBackground(GLOBAL_STATE.display, GLOBAL_STATE.gc, white);
+	XSetForeground(GLOBAL_STATE.display, GLOBAL_STATE.gc, black);
 
-	XMapRaised(dpy, win);
+	XMapRaised(GLOBAL_STATE.display, GLOBAL_STATE.win1);
 
 	struct Graph g = graph_new(WIDTH, HEIGHT);
 
+	XEvent event;
 	while (loop)
 	{
-		XFlush(dpy);
-		if (XEventsQueued(dpy, QueuedAfterFlush) != 0)
+		XFlush(GLOBAL_STATE.display);
+		if (XEventsQueued(GLOBAL_STATE.display, QueuedAfterFlush) != 0)
 		{
-			XNextEvent(dpy, &evt);
-			switch (evt.type)
+			XNextEvent(GLOBAL_STATE.display, &event);
+			switch (event.type)
 			{
 				case ClientMessage:
-					MESSAGE = evt.xclient.message_type;
+					MESSAGE = event.xclient.message_type;
 					loop = 0;
 					break;
 				case Expose:
-					XGetWindowAttributes(dpy, win, &attr);
-					XSetForeground(dpy, gc, black);
+					XGetWindowAttributes(GLOBAL_STATE.display, GLOBAL_STATE.win1, &attr);
+					XSetForeground(GLOBAL_STATE.display, GLOBAL_STATE.gc, black);
 					Xutf8TextExtents(font, str, strlen(str), &inkrect, &logrect);
 					extents = XExtentsOfFontSet(font);
-					Xutf8DrawString(dpy, win, font, gc,
+					Xutf8DrawString(GLOBAL_STATE.display, GLOBAL_STATE.win1, font, GLOBAL_STATE.gc,
 							(attr.width - logrect.width) / 2,
 							(attr.height - logrect.height) / 2 - extents->max_logical_extent.y,
 							str, strlen(str));
-					XFlush(dpy);
+					XFlush(GLOBAL_STATE.display);
 
                     // line_draw(graph_line);
 					draw_graph(g, x11_draw_line);
 					break;
 				case ButtonPress:
 					printf("ButtonPress\n");
-					printf("x = %d,  y = %d\n", evt.xbutton.x, evt.xbutton.y);
+					printf("x = %d,  y = %d\n", event.xbutton.x, event.xbutton.y);
 					break;
 				case ButtonRelease:
 					printf("ButtonRelease\n");
-					XGetWindowAttributes(dpy, win, &attr);
-					printf("x = %d,  y = %d\n", evt.xbutton.x, evt.xbutton.y);
+					XGetWindowAttributes(GLOBAL_STATE.display, GLOBAL_STATE.win1, &attr);
+					printf("x = %d,  y = %d\n", event.xbutton.x, event.xbutton.y);
 					// if (evt.xbutton.x > 0 && evt.xbutton.x < attr.width
 					// 		&& evt.xbutton.y > 0 && evt.xbutton.y < attr.height)
 					// 	loop = 0;
@@ -120,8 +130,8 @@ int main(int argc, char *argv[])
 		}
 	}
     graph_free(&g);
-	XFreeFontSet(dpy, font);
-	XDestroyWindow(dpy, win);
-	XCloseDisplay(dpy);
+	XFreeFontSet(GLOBAL_STATE.display, font);
+	XDestroyWindow(GLOBAL_STATE.display, GLOBAL_STATE.win1);
+	XCloseDisplay(GLOBAL_STATE.display);
     return 0;
 }
